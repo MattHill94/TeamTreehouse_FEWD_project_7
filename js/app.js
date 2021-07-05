cf160eda5b56c7646ea38adda217db9032b6ea9e
Matthew Hill <matty2712@hotmail.co.uk>
2021-07-05 13:40:11 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,71 +1,156 @@
-const alertContainer = document.querySelector('.alert-container');
-const alert = document.querySelector('.btn-alert');
-const dot = document.querySelector('.dot');
-const bell = document.querySelector('.header-icon');
-const messageFieldsHeight = document.querySelector('.message-fields').clientHeight;
-const userFields = document.querySelector('.user-fields');
-const dashboardSearch = document.querySelector('.dashboard-search');
-const magBG = document.querySelector('.mag-bg');
-const userSearch = document.querySelector('.user-search');
-const userMessage = document.querySelector('.user-message');
+const alertBanner = document.getElementById("alert");
+const notifications = document.getElementById("notifications");
+const popup = document.getElementById("popup");
+const dot = document.getElementById("dot");
+const user= document.getElementById("userField");
+const message = document.getElementById("messageField");
+const form = document.getElementById("form");
+let trafficNav = document.querySelector(".traffic-nav");
 
-// Removes the alert when "X" is clicked
-alert.addEventListener('click', () => {
-  alertContainer.innerHTML = "";
-  dot.style.display = "none";
-});
-
-// Displays pop-up windows
-bell.addEventListener('click', () => {
-  window.alert('Please read the alert in the banner below.');
-  window.alert('When you\'re done, click the X to close the alert.');
-});
-
-// Creates box shadow on dashboard-search:focus
-dashboardSearch.addEventListener('focus', () => {
-  magBG.style.boxShadow = '4px 0 4px 2px rgba(140, 140, 140, 0.2)';
-});
-
-// Removes box shadow after dashboard-search:blur
-dashboardSearch.addEventListener('blur', () => {
-  magBG.style.boxShadow = '';
-});
-
-// Sets height of user-fields div equal to height of message-fields div
-userFields.style.height = messageFieldsHeight + 'px';
-
-// Listens for submit event on any child element in user-fields (through event bubbling)
-userFields.addEventListener('submit', (e) => {
-  e.preventDefault();
-  let user = userSearch.value;
-  let message = userMessage.value;
-  if (user == '' && message == '') {
-    window.alert('You haven\'t entered a user or message.');
-  } else if (user == '') {
-    window.alert('You haven\'t entered a user.');
-  } else if (message == '') {
-    window.alert('You haven\'t entered a message.');
-  } else {
-    window.alert('Your message has been delivered.');
-  }
-});
-
-function myFunction() {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
-
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
+alertBanner.innerHTML =
+`<div class="alert-banner">
+    <p><strong>Alert:</strong> You have unread messages</p>
+    <p class="alert-banner-close"> X </p>
+    </div>
+`
+alertBanner.addEventListener('click', e =>{
+    const element = e.target;
+    if(element.classList.contains("alert-banner-close")){
+        alertBanner.style.display ="none"
     }
-  }
+});
+
+form.addEventListener('submit', e =>{
+    //let userName = user.options[user.selectedIndex].text;
+    console.log(user.value);
+    if(user.value === "" && message.value=== ""){
+        alert("Please fill out user and messages fields before sending");
+        e.preventDefault();
+    }else if(user.value === ""){
+        alert("Please fill out user field before sending");
+        e.preventDefault();
+    }else if(message.value === ""){
+        alert("Please fill out message field before sending");
+        e.preventDefault();
+    }else{
+        alert(`Message succesfully sent to: ${user.value}`)
+    }
+} );
+
+//create messages, but do not show
+let messageShown;
+
+function createMessages(){
+    if(popup.childNodes.length==0 && dot.style.display !== "none"){
+        for(let i=0; i<2; i++){
+            let message = document.createElement('div');
+        message.className = "popup-message";
+        message.innerHTML =
+        `
+        <p>You have a message from Dawn</p>
+        <p class="message-close">X</p>   
+        `;
+        popup.appendChild(message);
+        }
+    }
+    popup.style.display = "none";
 }
+createMessages();
+
+
+notifications.addEventListener('click', e =>{
+    const element = e.target;
+    //delete dot, when messages show for first time
+    dot.style.display = "none";
+    //only clicking on messages or bell should hide/show messages
+    if(element.className!=="message-close"){
+        if(!messageShown){
+            popup.style.display = "block";
+            messageShown=true;
+        }
+        else{
+            popup.style.display = "none";
+            messageShown= false;
+        }
+
+    }
+});
+
+popup.addEventListener('click', e =>{
+    const element = e.target;
+
+    if(element.classList.contains("message-close")){
+        const parent = element.parentNode;
+        parent.parentNode.removeChild(parent);
+    }
+});
+
+trafficNav.addEventListener('click', e =>{
+    const element = e.target;
+    const datatype = element.textContent;
+    if(element.className ===  "traffic-nav-link" || element.className ===  "traffic-nav-link active"){
+        //set active button inactive
+        let lis = document.querySelectorAll(".traffic-nav-link");
+        for(let i=0; i<lis.length; i++){
+            let activeButton = lis[i];
+            if(activeButton.className==="traffic-nav-link active"){
+                activeButton.className="traffic-nav-link"
+            }
+        }
+        //set clicked button to active if not already active
+        if(element.className==="traffic-nav-link"){
+         element.className = "traffic-nav-link active";
+        }
+
+        //change graph
+            updateTrafficData(datatype);
+            updateChart(trafficChart, trafficData);
+        }
+    }
+);
+
+//saving settings to localStorage
+const emailCheckbox = document.getElementById("emailSettings");
+const publicCheckbox = document.getElementById("publicSettings");
+const timeZone = document.getElementById("timezone");
+const saveButton = document.getElementById("save");
+const cancelButton = document.getElementById("cancel");
+
+//set default settings
+if(localStorage.length != 0){
+    localStorage.getItem('emailNotificationsOn') === "false"?
+        emailCheckbox.checked = false:
+        emailCheckbox.checked = true;
+
+    localStorage.getItem('profilePublic') === "false"?
+        publicCheckbox.checked = false:
+        publicCheckbox.checked = true;
+
+   timeZone.value = localStorage.timeZone
+}
+
+
+saveButton.addEventListener('click', () => {
+    localStorage.setItem('emailNotificationsOn', emailCheckbox.checked );
+    localStorage.setItem('profilePublic', publicCheckbox.checked);
+    localStorage.setItem('timeZone', timeZone.value );
+});
+
+cancelButton.addEventListener('click', ()=>{
+    emailCheckbox.checked=false;
+    publicCheckbox.checked=false;
+    timeZone.value = 'default';
+    localStorage.clear();
+    location.reload();
+
+});
+
+
+
+
+
+
+
+
+
+
